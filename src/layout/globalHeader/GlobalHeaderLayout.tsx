f28326f63205d21add91c73862c52ebf3607da52
ShinJaeIl01 <ekvm555@naver.com>
2022-11-03 15:40:42 +0900

    feat: Create Link Header Layout #1

--- src/layout/globalHeader/GlobalHeaderLayout.tsx
+++ src/layout/globalHeader/GlobalHeaderLayout.tsx
@@ -1,5 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 export const GlobalHeaderLayout = () => {
-  return <div>GlobalHeaderLayout</div>
+  return (
+    <HeaderContainer>
+      <Link to="/">
+        <Text>INFO</Text>
+      </Link>
+      <Link to="/">
+        <Text>ARTWORK</Text>
+      </Link>
+      <Link to="/">
+        <Text>ARTIST</Text>
+      </Link>
+    </HeaderContainer>
+  )
 }
+
+const HeaderContainer = styled.div`
+  background-color: #242528;
+  height: 3.12rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 3.12rem;
+`
+const Text = styled.span`
+  color: #fff;
+`
