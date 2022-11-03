2183b2b6c7aba217246e3fc59fa9e82a60149ba6
ShinJaeIl01 <ekvm555@naver.com>
2022-11-03 21:54:50 +0900

    feat: Create Change Text Color of Info Header #2

--- src/components/info/Info.tsx
+++ src/components/info/Info.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import styled from 'styled-components'
 import { InfoCreditLayout } from '../../layout/info/InfoCreditLayout'
@@ -8,9 +8,47 @@ import { InfoHowLayout } from '../../layout/info/InfoHowLayout'
 import { InfowwwLayout } from '../../layout/info/InfowwwLayout'
 
 export const Info = () => {
+  // 헤더 텍스트 컬러 변경
+  const [textColor, setTextColor] = useState([true, false, false, false])
+  const textColorHandler = (page: string) => {
+    /* eslint-disable */
+    switch (page) {
+      case 'www':
+        setTextColor((prev) => {
+          const next = prev.map((item) => (item = false))
+          next[0] = true
+          return next
+        })
+        break
+      case 'how':
+        setTextColor((prev) => {
+          const next = prev.map((item) => (item = false))
+          next[1] = true
+          return next
+        })
+        break
+      case 'credit':
+        setTextColor((prev) => {
+          const next = prev.map((item) => (item = false))
+          next[2] = true
+          return next
+        })
+        break
+      case 'font':
+        setTextColor((prev) => {
+          const next = prev.map((item) => (item = false))
+          next[3] = true
+          return next
+        })
+        break
+      default:
+        return
+    }
+    /* eslint-enable */
+  }
   return (
     <Background>
-      <InfoHeaderLayout />
+      <InfoHeaderLayout textColor={textColor} textColorHandler={textColorHandler} />
       <Routes>
         <Route path="/www" element={<InfowwwLayout />} />
         <Route path="/how" element={<InfoHowLayout />} />
--- src/layout/info/InfoHeaderLayout.tsx
+++ src/layout/info/InfoHeaderLayout.tsx
@@ -2,20 +2,39 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
-export const InfoHeaderLayout = () => {
+/* eslint-disable */
+interface InfoHeaderProps {
+  textColor: boolean[]
+  textColorHandler: (page: string) => void
+}
+/* eslint-enable */
+
+export const InfoHeaderLayout = ({ textColor, textColorHandler }: InfoHeaderProps) => {
   return (
     <HeaderContainer>
       <LinkBox>
-        <Text to="www">www</Text>
+        <Text to="www" color={textColor[0] ? '#64E269' : '#242528'} onClick={() => textColorHandler('www')}>
+          www
+        </Text>
       </LinkBox>
       <LinkBox>
-        <Text to="how">HOW</Text>
+        <Text to="how" color={textColor[1] ? '#64E269' : '#242528'} onClick={() => textColorHandler('how')}>
+          HOW
+        </Text>
       </LinkBox>
       <LinkBox>
-        <Text to="credit">CREDIT</Text>
+        <Text
+          to="credit"
+          color={textColor[2] ? '#64E269' : '#242528'}
+          onClick={() => textColorHandler('credit')}
+        >
+          CREDIT
+        </Text>
       </LinkBox>
       <LinkBox>
-        <Text to="font">FONT</Text>
+        <Text to="font" color={textColor[3] ? '#64E269' : '#242528'} onClick={() => textColorHandler('font')}>
+          FONT
+        </Text>
       </LinkBox>
     </HeaderContainer>
   )
@@ -29,9 +48,10 @@ const HeaderContainer = styled.div`
 const LinkBox = styled.div`
   padding: 0 2.75rem;
 `
-const Text = styled(Link)`
+const Text = styled(Link)<{ color: string }>`
   font-size: 1rem;
   font-weight: 500;
   line-height: 1.2rem;
   letter-spacing: -0.02rem;
+  color: ${({ color }) => color};
 `
