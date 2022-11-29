8340ab60a5f3873a16df5871ac0fd0140706b172
ShinJaeIl01 <ekvm555@naver.com>
2022-11-29 21:54:26 +0900

    feat: Env Variables Replacement in index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            naverClientId: env.VITE_NAVER_MAP_CLIENT_ID
+          }
+        }
+      })
+    ]
+  }
 })
