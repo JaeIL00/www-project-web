f0e78e963c04adb7f736b90a6077ebb9582c931f
ShinJaeIl01 <ekvm555@naver.com>
2022-12-14 15:42:38 +0900

    feat: Favicon and Site Name #31

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import { Route, Routes, useNavigate } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import { GlobalHeader } from './components/globalHeader/GlobalHeader'
 import { ArtistPage } from './pages/ArtistPage'
 import { ArtworkPage } from './pages/ArtworkPage'
